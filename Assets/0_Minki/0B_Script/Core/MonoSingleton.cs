1fdd3c8f68231a55a07c0e9403f5ebc541e5c7be
mki821 <alexalex0821@naver.com>
2024-11-01 03:02:51 +0900

    angle

--- Assets/0_Minki/0B_Script/Core/MonoSingleton.cs
+++ Assets/0_Minki/0B_Script/Core/MonoSingleton.cs
@@ -24,8 +24,11 @@ public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T
     protected virtual void Awake() {
         if(_instance == null) {
             _instance = this as T;
-            DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+    }
+
+    protected virtual void OnDestroy() {
+        if(_instance == this) _instance = null;
     }
 }
--- Assets/0_Minki/0B_Script/FSM/Player/PlayerRope.cs
+++ Assets/0_Minki/0B_Script/FSM/Player/PlayerRope.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerRope : MonoBehaviour
@@ -7,21 +8,21 @@ public class PlayerRope : MonoBehaviour
     
     [SerializeField] private float _ropeDistance;
     [SerializeField] private float _ropeSpeed = 2f;
+    [SerializeField] private Transform _grabTrm;
     [SerializeField] private LayerMask _whatIsRopeable;
     [SerializeField] private LayerMask _whatIsEnemy;
 
     private bool _isGrab = false;
+    private Vector2 _originPosition;
 
-    private LineRenderer _lineRenderer;
+    private Coroutine _coroutine = null;
 
     private Player _player;
 
-    private void Awake() {
-        _lineRenderer = GetComponentInChildren<LineRenderer>();
-    }
-
     public void Initialize(Player player){
         _player = player;
+
+        _originPosition = _grabTrm.localPosition;
 
         _player.Input.RopeEvent += Rope;
         _player.Input.RopeCancelEvent += RopeCancel;
@@ -32,41 +33,125 @@ public class PlayerRope : MonoBehaviour
         _player.Input.RopeCancelEvent -= RopeCancel;
     }
 
-    private void Update() {
-        if(_isGrab) {
-            _lineRenderer.SetPosition(0, _player.transform.position);
-            _lineRenderer.SetPosition(1, anchorPosition);
-        }
-    }
-
     private void Rope() {
         Vector2 direction = _player.Input.MouseWorldPos - (Vector2)transform.position;
         float distance = Mathf.Min(direction.magnitude, _ropeDistance);
 
-        direction.Normalize();
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, distance, _whatIsRopeable | _whatIsEnemy);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, distance, _whatIsRopeable | _whatIsEnemy);
 
         if(hit.collider != null) {
             if((1 << hit.collider.gameObject.layer) == _whatIsRopeable.value) {
                 anchorPosition = hit.point;
-                _isGrab = true;
-                
-                _player.StateMachine.ChangeState(PlayerStateEnum.Rope);
+
+                if(_coroutine != null) StopCoroutine(_coroutine);
+                _coroutine = StartCoroutine(GrabCoroutine());
             }
             else if(hit.collider.TryGetComponent(out Enemy enemy)) {
                 targetEnemy = enemy;
-                targetEnemy.Catched();
+                
+                if(_coroutine != null) StopCoroutine(_coroutine);
+                _coroutine = StartCoroutine(GrabCoroutineEnemy());
             }
         }
+        else {
+            if(_coroutine != null) StopCoroutine(_coroutine);
+            _coroutine = StartCoroutine(GrabCoroutine((Vector2)transform.position + direction));
+        }
+    }
+
+    private IEnumerator GrabCoroutine() {
+        Vector2 startPosition = (Vector2)transform.position + _originPosition;
+        float timer = 0f;
+        _grabTrm.SetParent(null);
+
+        Vector2 direction = anchorPosition - startPosition;
+        direction.Normalize();
+        
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _grabTrm.rotation = Quaternion.Euler(0, 0, angle - 45f);
+
+        while(timer < 1f) {
+            timer += Time.deltaTime * 5f;
+
+            Vector2 currentPosition = Vector2.Lerp(startPosition, anchorPosition, timer);
+            _grabTrm.position = currentPosition;
+
+            yield return null;
+        }
+        
+        _isGrab = true;
+        _player.StateMachine.ChangeState(PlayerStateEnum.Rope);
+    }
+
+    private IEnumerator GrabCoroutine(Vector2 targetPosition) {
+        Vector2 startPosition = (Vector2)transform.position + _originPosition;
+        float timer = 0f;
+        _grabTrm.SetParent(null);
+
+        Vector2 direction = targetPosition - startPosition;
+        direction.Normalize();
+        
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _grabTrm.rotation = Quaternion.Euler(0, 0, angle - 45f);
+
+        while(timer < 1f) {
+            timer += Time.deltaTime * 5f;
+
+            Vector2 currentPosition = Vector2.Lerp(startPosition, targetPosition, timer);
+            _grabTrm.position = currentPosition;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator GrabCoroutineEnemy() {
+        Vector2 startPosition = (Vector2)transform.position + _originPosition;
+        float timer = 0f;
+        _grabTrm.SetParent(null);
+
+        Vector2 direction = (Vector2)targetEnemy.transform.position - startPosition;
+        direction.Normalize();
+        
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _grabTrm.rotation = Quaternion.Euler(0, 0, angle - 45f);
+
+        while(timer < 1f) {
+            timer += Time.deltaTime * 5f;
+
+            Vector2 currentPosition = Vector2.Lerp(startPosition, targetEnemy.transform.position, timer);
+            _grabTrm.position = currentPosition;
+
+            yield return null;
+        }
+        
+        _isGrab = true;
+        targetEnemy.Catched();
     }
 
     private void RopeCancel() {
         _isGrab = false;
-        _lineRenderer.SetPosition(0, Vector2.zero);
-        _lineRenderer.SetPosition(1, Vector2.zero);
 
-        targetEnemy.isCatchCanceled = true;
+        if(_coroutine != null) StopCoroutine(_coroutine);
+        _coroutine = StartCoroutine(ComeBackCoroutine());
+
+        if(targetEnemy != null)
+            targetEnemy.isCatchCanceled = true;
+    }
+
+    private IEnumerator ComeBackCoroutine() {
+        Vector2 startPosition = _grabTrm.position;
+        float timer = 0f;
+
+        while(timer < 1f) {
+            timer += Time.deltaTime * 5f;
+
+            Vector2 currentPosition = Vector2.Lerp(startPosition, (Vector2)transform.position + _originPosition, timer);
+            _grabTrm.position = currentPosition;
+
+            yield return null;
+        }
+        _grabTrm.SetParent(transform);
+        _grabTrm.rotation = Quaternion.identity;
     }
 
     private void OnDrawGizmos() {
